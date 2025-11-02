edd9e0be4fa8f44341b60ac692f13405b0015442
Douglas Fragoso <douglas.iff@gmail.com>
2025-11-02 19:26:37 -0300

    docs: Spring Boot Initializr

---
+++ ia/src/test/java/com/langchain4j/ia/IaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.langchain4j.ia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
